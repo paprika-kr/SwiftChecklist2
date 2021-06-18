ce1086abdf5cd76096154cf4023e06452b3c12fc
Dong-Hyeon Kim <feira@naver.com>
2021-06-18 11:36:49 +0900

    Finish style7

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 
-class AddItemViewController: UITableViewController {
+class AddItemViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var doneBarButton:
+        UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,6 +47,23 @@ class AddItemViewController: UITableViewController {
         return nil
     }
     
+    
+    
+    //MARK:- Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let oldText = textField.text!
+        let stringRange = Range(range, in:oldText)!
+        let newText = oldText.replacingCharacters(in: stringRange, with: string)
+        doneBarButton.isEnabled = !newText.isEmpty
+        return true
+    }
+    
+    /* Clear Button */
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        doneBarButton.isEnabled = false
+        return true
+    }
+    
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
@@ -52,6 +72,7 @@ class AddItemViewController: UITableViewController {
 
         return cell
     }
+    
     */
 
     /*
