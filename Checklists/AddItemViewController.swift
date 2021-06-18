d3a819da1ff0fd4fd5cdef832684c58754336ee6
Dong-Hyeon Kim <feira@naver.com>
2021-06-18 10:55:07 +0900

    finish staticTable 6

--- Checklists/AddItemViewController.swift
+++ Checklists/AddItemViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class AddItemViewController: UITableViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,13 +21,27 @@ class AddItemViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
+    /* Initial Keyboard*/
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        textField.becomeFirstResponder()
+    }
+    
     //MARK:- Actions
     @IBAction func cancel() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func done() {
+        print("Contents of the text field: \(textField.text!)")
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK:- Table View Delegates
+    override func tableView(_ tableView: UITableView,
+                            willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
     
     /*
